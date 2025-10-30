f4e252a33be093a415d163d08ddd0114e72322f6
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-10-30 16:50:06 +0000

    Work

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,4 +4,6 @@ from .models import Book
 @admin.register(Book)   
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author']
+    list_filter = ['publication_year']
 
