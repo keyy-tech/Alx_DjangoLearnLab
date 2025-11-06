6840cfa73f4845f666d81121732003095cc5240d
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-11-06 22:48:44 +0000

    Still editing

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,12 +5,12 @@ from django.views.generic import DetailView, ListView
 
 def list_all_books(request):
     books = Book.objects.select_related("author").all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
 class DetailLibraryList(DetailView):
     model = Book
-    template_name = "book_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
     def get_context_data(self, **kwargs):
