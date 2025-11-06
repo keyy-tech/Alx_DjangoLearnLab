c280f8f410a0d160ae70e1c49155625b65f0bc82
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-11-06 21:24:36 +0000

    Done

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -21,3 +21,9 @@ def query_books_by_a_specific_author(author_name):
     author = Author.objects.get(name=author_name)
     book_author = Book.objects.filter(author=author)
     return book_author
+
+
+def retrieve_the_librarian_for_a_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = Librarian.objects.get(library=library)
+    return librarian
