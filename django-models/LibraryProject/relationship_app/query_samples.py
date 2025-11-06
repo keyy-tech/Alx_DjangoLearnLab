e1f340e314970561b045a5c50c562a29fd71c699
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-11-06 21:14:24 +0000

    Working

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,5 +3,13 @@ from relationship_app.models import Author, Library, Librarian, Book
 Book.objects.select_related("author")
 
 
-library = Library.objects.prefetch_related('books').get(name="library_name")
+library = Library.objects.prefetch_related("books").get(name="library_name")
 books = library.books.all()
+
+
+from relationship_app.models import Library
+
+def list_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    return books
