4e1434ac4df7875a1604ed64675d1c6fb11c77a8
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-11-06 21:22:00 +0000

    Working on query_samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,7 +9,15 @@ books = library.books.all()
 
 from relationship_app.models import Library
 
+
 def list_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
     books = library.books.all()
     return books
+
+
+
+def query_books_by_a_specific_author(author_name):
+    author = Author.objects.get(name=author_name)
+    book_author = Book.objects.filter(author=author)
+    return book_author
