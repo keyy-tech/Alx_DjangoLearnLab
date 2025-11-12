6342ac97aa07506b437e32a2787cd2b1a985fd3a
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-11-12 00:01:28 +0000

    Use BaseUserManager for CustomUserManager
    Replaced inheritance from UserManager with BaseUserManager in CustomUserManager to better align with Django's recommended custom user model practices. This change ensures proper handling of user creation and management when using a custom user model.

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import UserManager
 
 
-class CustomUserManager(UserManager):
-    def create_user(self,email,password=None, **extra_fields):
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Email is required")
         user = self.model(email=self.normalize_email(email), **extra_fields)
@@ -12,11 +13,11 @@ class CustomUserManager(UserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self,email,password=None,**extra_fields):
-        extra_fields.setdefault("is_staff",True)
-        extra_fields.setdefault("is_superuser",True)
-        extra_fields.setdefault("is_active",True)
-        return self.create_user(email,password,**extra_fields)
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        return self.create_user(email, password, **extra_fields)
 
 
 class Book(models.Model):
