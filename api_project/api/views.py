0bbd9cb4c5d0b7a594f4e0081c738ad4bd3aa27b
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-11-17 20:40:44 +0000

    Add BookViewSet and register with router
    Introduced BookViewSet using ModelViewSet for the Book model and registered it with a DRF DefaultRouter in urls.py. This enables RESTful endpoints for books in addition to the existing BookList view.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,16 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.viewsets import ModelViewSet
 
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
