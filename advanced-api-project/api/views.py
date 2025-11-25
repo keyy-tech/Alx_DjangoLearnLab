75d99544efe544d2e83e4ccd9f670ce7cdd8b751
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-11-25 18:41:03 +0000

    Fix import of DjangoFilterBackend in views.py
    Updated the import statement to use 'from django_filters import rest_framework' and referenced DjangoFilterBackend via rest_framework to ensure correct usage with DRF filtering.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .serializers import BookSerializer, AuthorSerializer
@@ -11,7 +11,7 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ["publication_year"]
     search_fields = [
         "title",
