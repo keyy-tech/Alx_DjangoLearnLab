04706ffecd62bee8533f75c3d62b21e2b40e0290
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-12-07 21:29:04 +0000

    Refactor blog app and add users app with profile model
    Removed login and register templates from the blog app and introduced a new users app with a UserProfile model, admin integration, and user-related views. Refactored blog forms to use PostForm, added CRUD views and templates for posts, and updated URL routing to include both users and blog apps. This sets up a clearer separation between user management and blog post functionality.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,24 +1,15 @@
-from calendar import c
-from tkinter import N
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
 
 
-class RegisterForm(UserCreationForm):
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ["username", "email", "password1", "password2"]
+        model = Post
+        fields = "__all__"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for fields in self.fields.values():
-            fields.widget.attrs.update({"class": "form-control"})
-            fields.help_text = None
-
-
-class LoginForm(AuthenticationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for fields in self.fields.values():
-            fields.widget.attrs.update({"class": "form-control"})
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
