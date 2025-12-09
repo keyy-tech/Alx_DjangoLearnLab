88da2c3bfd300f0a207ad499a3b19f13fba2e8af
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-12-09 15:12:14 +0000

    Move user functionality into blog app and update templates
    All user-related code, templates, and tests have been moved from the 'users' app to the 'blog' app. The 'users' app and its models, forms, views, and migrations have been deleted. Template paths have been updated and redundant 'blog/' prefixes removed from template inheritance. The settings have been updated to use the new template directory and remove the 'users' app from INSTALLED_APPS.

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog.apps.BlogConfig",
-    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -56,7 +54,7 @@ ROOT_URLCONF = "django_blog.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "blog/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
